3bece6c2cdaeb78207be6a9dfa85cd4ef8e64afc
president <abmcompanysn@gmail.com>
2025-10-18 12:06:31 +0000

    Ajout des modifications sur junior-senior

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // URL de l'API pour la gestion des comptes (authentification, etc.)
-    ACCOUNT_API_URL: "https://script.google.com/macros/s/AKfycbx_eVTGb3WF3oX_DmZ7_VR-_ww0R2JIfuvGv4GiwvIUCefIXOLMBWFsmihFRWeoSBwO/exec",
+    ACCOUNT_API_URL: "https://script.google.com/macros/s/AKfycbya0cKhT_WePULg9q6mAz-C_80hpZtBz4Su2ebs76YZ2wErWCp9kAM9N2YmfXbU9vcywA/exec",
     // NOUVEAU: URL de l'API centrale pour la gestion des cours, achats, et progression
     COURSE_API_URL: "https://script.google.com/macros/s/AKfycbzQk4CwkPid9WBuRFbI-QUW2MZvLxV-ke0g--3uvIBj5s82_1zhBBZoUFEtz7sqDHxi0g/exec",
     // NOUVEAU: URL de l'API dédiée aux notifications
